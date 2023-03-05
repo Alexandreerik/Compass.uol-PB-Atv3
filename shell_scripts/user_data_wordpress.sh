ed264c6f059cb35c56b648e14433317806db4135
Erik Alexandre <alexandre1erik@gmail.com>
2023-03-05 10:06:05 -0300

    Adicionando user data da instância wordpress

---
+++ shell_scripts/user_data_wordpress.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Instalação e configuração do Docker
+yum update -y
+yum install docker -y
+systemctl start docker
+systemctl enable docker
+usermod -a -G docker ec2-user
+
+# Instalação do docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Montagem do efs
+mkdir -p /mnt/nfs
+echo "172.31.11.203:/ /mnt/nfs nfs defaults 0 0" >> /etc/fstab
+mount -a
+
+# Executando o docker-compose do repositorio
+yum install git -y
+git clone https://github.com/Alexandreerik/Compass.uol-PB-Atv3.git /home/ec2-user/atividade_aws_docker
+docker-compose -f /home/ec2-user/Compass.uol-PB-Atv3/docker-compose.yml up -d
